696f7e47ac6fa1fbc12884c8121e246ca8500a2e
verissimo francis <verissimo francis>
2022-01-20 10:10:45 +0100

    Update src folder
    -Add function.js file for remove and edit functionality
    -Add storage.js file for localstorage
    -Update index.js file with add button and import file

---
+++ src/function.js
@@ -0,0 +1,34 @@
+const apply = (listTask, container) => {
+  const sortedTodos = listTask.list.sort((a, b) => a.index - b.index);
+  container.innerHTML = '';
+  let todosHtml = '';
+  sortedTodos.forEach((todo) => {
+    todosHtml += `  <div class="item">
+                              <div>
+                                  <input id="${todo.id}" type="checkbox" class="checkbox" />
+                                  <input id="${todo.index}" type="text" class="todo-edit" value="${todo.description}" />
+                            </div>
+                        <i id="${todo.index}" class="remove fas fa-trash"></i>
+                    </div>
+          `;
+  });
+
+  container.innerHTML = todosHtml;
+
+  const removeBtns = document.querySelectorAll('.remove');
+  removeBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      const element = btn.parentNode;
+      listTask.RemoveTask(Number(e.target.parentNode.id));
+      element.remove();
+    });
+  });
+
+  const todosContent = document.querySelectorAll('.todo-edit');
+  todosContent.forEach((todo) => {
+    todo.addEventListener('change', (e) => {
+      listTask.EditTask(Number(e.target.id), e.target.value);
+    });
+  });
+};
+export default apply;
