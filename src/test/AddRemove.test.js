b0ec5c50495e4132df78a292fc6d5c98fbc163ee
verissimo francis <verissimo francis>
2022-02-02 14:42:17 +0100

    Add function for Editing, Updating, and Clear all completed

--- src/test/AddRemove.test.js
+++ src/test/AddRemove.test.js
@@ -53,3 +53,44 @@ describe('add and remove', () => {
     expect(todoList.list).toHaveLength(2);
   });
 });
+
+describe('Edit test', () => {
+  test('Editing', () => {
+    const todoList = new Task();
+    const newTodo6 = {
+      id: 'gvjvuyy',
+      description: 'asd',
+      completed: false,
+      index: 3,
+    };
+    todoList.addTask(newTodo6);
+    todoList.EditTask(newTodo6.id, 'asd');
+    expect(todoList.list[2].description).toBe('asd');
+    expect(todoList.list).toHaveLength(3);
+  });
+});
+
+describe('complete test', () => {
+  test(' updating an items completed status', () => {
+    const todoList = new Task();
+    const newTodo8 = {
+      id: 'dafcfrdy',
+      description: 'task9',
+      completed: false,
+      index: 4,
+    };
+    todoList.addTask(newTodo8);
+    todoList.CompleteTask(newTodo8.id, true);
+    expect(todoList.list[3].completed).toBeTruthy();
+    expect(todoList.list).toHaveLength(4);
+  });
+});
+
+describe('Clear all completed', () => {
+  test('clear items completed', () => {
+    const todoList = new Task();
+    todoList.clearCompletedTasks();
+    expect(todoList.list).toHaveLength(3);
+    expect(todoList.list[1].completed).toBeFalsy();
+  });
+});
